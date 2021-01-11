45e3b73a3adc1f2a9bd957415205a2d905ca2ed0
Michael Stringer Jr <michael.stringer.2.0@gmail.com>
2021-01-11 09:28:30 -0600

    Create review.py

---
+++ review.py
@@ -0,0 +1,45 @@
+# A Program to determine employee eligability for advancement
+
+# Created by: <your name here>
+
+# Copyright CTHS Engineering, Inc., 2021
+# This code or any portion fo this code can be be reused without 
+#    previous approval from the company CIO or CEO, in writing.
+
+empName = "Sam"
+
+#Project1(P1) - New school wing
+#TA - Task Accuracy
+#EstBud - Estimated Budget
+#ActBud - Actual Budget
+#EstMP - Estimated Manpower
+#ActMP - Actual Manpower
+empP1TA = 92
+empP1EstBud = 1285000
+empP1ActBud = 1301346
+empP1EstMP = 1625
+empP1EstMP = 1650
+
+#Project2 - Custom motorcycle company warehouse
+empP2TA = 98
+empP2EstBud = 650000
+empP2ActBud = 624000
+empP2EstMP = 525
+empP2ActMP = 515
+
+#Project3 - Minor Nascar training track
+empP3TA = 96
+empP3EstBud = 2500000
+empP3ActBud = 3231325
+empP3EstMP = 1050
+empP3ActMP = 1250
+
+#Project4 - Man cave warehouse and house
+empP4TA = 92
+empP4EstBud = 825000
+empP4ActBud = 830000
+empP4EstMP = 400
+empP4ActMP = 375
+
+#your code goes below
+
