34b92903b8920a3682a3c8bc43cd7f82c5067cef
Bn0280924 <69972405+Bn0280924@users.noreply.github.com>
2021-01-20 08:59:56 -0600

    Update review.py

--- review.py
+++ review.py
@@ -18,7 +18,7 @@ empP1TA = 92
 empP1EstBud = 1285000
 empP1ActBud = 1301346
 empP1EstMP = 1625
-empP1EstMP = 1650
+empP1ActMP = 1650
 
 #Project2 - Custom motorcycle company warehouse
 empP2TA = 98
@@ -42,4 +42,36 @@ empP4EstMP = 400
 empP4ActMP = 375
 
 #your code goes below
+tenpercent1=empP1EstBud-empP1ActBud/empP1EstBud 
+manpower1=empP1ActMP-empP1EstMP/empP1EstMP
+if empP1TA >= 90 and tenpercent1 <= 0.10 and manpower1 <= 0.25:
+    print("Everything is right ")
+else:
+    print("No one or more reason is wrong")
+
+tenpercent2=empP2EstBud-empP2ActBud/empP2EstBud 
+manpower2=empP2ActMP-empP2EstMP/empP2EstMP
+if empP2TA >= 90 and tenpercent2 <= 0.10 and manpower2 <= 0.25:
+    print("Everything is right ")
+else:
+    print("No one or more reason is wrong")
+
+tenpercent3=empP3EstBud-empP3ActBud/empP3EstBud 
+manpower3=empP3ActMP-empP3EstMP/empP3EstMP
+if empP3TA >= 90 and tenpercent3 <= 0.10 and manpower3 <= 0.25:
+    print("Everything is right ")
+else:
+    print("No one or more reason is wrong")
+
+tenpercent4=empP4EstBud-empP4ActBud/empP4EstBud 
+manpower4=empP4ActMP-empP4EstMP/empP4EstMP
+if empP4TA >= 90 and tenpercent4 <= 0.10 and manpower4 <= 0.25:
+    print("Everything is right ")
+else:
+    print("No one or more reason is wrong")
+    
+            
+
+
+
 
